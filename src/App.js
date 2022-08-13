e4060100f1adeb28a3c7648d06e0dd3b12a0fc4b
YesYourShin <ehrl827@naver.com>
2022-08-14 02:58:41 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 // import './App.css';
+import Counter from './Counter';
 import MyHeader from './MyHeader';
 
 function App() {
-    const word = 'Hello World';
-
-    const style = {
-        App: {
-            backgroundColor: 'green',
-        },
-        h2: {
-            color: 'yellow',
-        },
-        bold_text: {
-            color: 'purple',
-        },
-    };
-
-    const number = 6;
-
     return (
-        <div className="App" style={style.App}>
+        <div className="App">
             <MyHeader />
-            <h2 style={style.h2}>{word}</h2>
-            <b id="bold_text" style={style.bold_text}>
-                {number}는 : {number % 2 === 0 ? '짝수' : '홀수'}
-            </b>
+            <Counter />
         </div>
     );
 }
